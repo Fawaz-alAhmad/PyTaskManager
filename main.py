775e481abebbab6616d61256203e6de4736f0204
Fawaz Al Ahmad <fawaz.amd11@gmail.com>
2024-04-01 22:00:02 +0300

    defined Task class with setters and getters

---
+++ main.py
@@ -0,0 +1,31 @@
+class Task:
+
+    __current_id= 0
+
+    def __init__(self,description,priority):
+        Task.__current_id += 1
+
+        self._task_id: int = Task.__current_id
+        self._description : str = description
+        self._priority: int = priority
+        self._completed: bool = False
+        self._next: Task | None = None
+
+    def getDescription(self):
+        return self._description
+    
+    def getPriority(self):
+        return self._priority
+    
+    def getCompleted(self):
+        return self._completed
+    
+    def getNext(self):
+        return self._next
+    
+    def setNext(self,task):
+        self._next = task
+
+    def markAsCompleted(self):
+        self._completed = True        
+    
